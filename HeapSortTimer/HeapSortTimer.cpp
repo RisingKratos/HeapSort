0a6dd0362531e084453e0a7c7ee7adde8d781c83
RisingKratos <risingkratos@gmail.com>
2015-10-14 14:39:44 +0600

    LOL

---
+++ HeapSortTimer/HeapSortTimer.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <time.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int arr[200000000];
+
+void heapify(int pos, int n) {
+	while (2 * pos + 1 < n) {
+
+		int t = 2 * pos + 1;
+		if (2 * pos + 2 < n && arr[2 * pos + 2] >= arr[t])
+		{
+			t = 2 * pos + 2;
+		}
+		if (arr[pos] < arr[t]) {
+			swap(arr[pos], arr[t]);
+			pos = t;
+		}
+		else break;
+
+	}
+}
+
+void BuildHeap(int n)
+{
+	for (int i = n - 1; i >= 0; i--)
+	{
+		heapify(i, n);
+	}
+}
+void heapSort(int n)
+{
+	BuildHeap(n);
+	while (n>0)
+	{
+		swap(arr[0], arr[n - 1]);
+		n--;
+		heapify(0, n);
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	int n;
+	cin >> n;
+	//test on the machine
+	//n approximately is 500000
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand()%100000;
+	}
+	clock_t time = clock();
+	heapSort(n);
+	time = clock() - time;
+	cout << endl << (double)time / CLOCKS_PER_SEC << endl;
+	return 0;
+}
